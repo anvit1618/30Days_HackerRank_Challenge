e9ab7830d5feb1674b9419638ee0407d99cdbc1f
Anvit Agarwal <71594140+anvit1618@users.noreply.github.com>
2022-08-28 21:25:30 +0530

    Create Day1.py

---
+++ Day1.py
@@ -0,0 +1,19 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+w = int
+r = float
+t = str
+# Read and save an integer, double, and String to your variables.
+w = int(input())
+t = float(input())
+r = input()
+
+# Print the sum of both integer variables on a new line.
+print(w+i)
+# Print the sum of the double variables on a new line.
+print(t+d)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s+r)
